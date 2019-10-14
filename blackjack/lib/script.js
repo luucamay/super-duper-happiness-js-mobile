76fd92e642551625f06dd763a9ea6d52ea23891e
luucamay <lupemaydana@gmail.com>
2019-10-14 17:15:48 -0400

    Solving typo bugs

--- blackjack/lib/script.js
+++ blackjack/lib/script.js
@@ -75,14 +75,10 @@ function getNextCard(){
     return deck.shift()
 }
 
-let deck = createDeck()
-
 for (let i = 0; i < deck.length; i++){
     console.log(deck[i])
 }
-let playerCards = [ getNextCard(), getNextCard()]
+
 console.log("Welcome to Blackjack!")
 
 console.log("You are dealt: ")
-console.log(" " + getCardString(playerCards[0]))
-console.log(" " + getCardString(playerCards[1]))
