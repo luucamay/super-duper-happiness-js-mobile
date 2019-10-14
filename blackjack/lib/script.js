3eb4463c42dedcb7d240eadd2c8a8ca5ffb2b79a
luucamay <lupemaydana@gmail.com>
2019-10-14 12:00:59 -0400

    Adding Loops to BlackJack

--- blackjack/lib/script.js
+++ blackjack/lib/script.js
@@ -1,11 +1,19 @@
 
-let deck = [
-    "Ace of Spades",
-    "Two of Spades",
-    "Three of Spades"
-]
-let playerCards = [ deck[0], deck[2]]    
+let suits = ["Hearts", "Clubs", "Diamonds", "Spiders"]
+let values = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One"]
 
+let deck = []
+
+for(let i = 0; i < suits.length; i++){
+    for(let j = 0; j < values.length; j++){
+        deck.push(values[j] + " of " + suits[i])
+    }
+}
+
+for (let i = 0; i < deck.length; i++){
+    console.log(deck[i])
+}
+let playerCards = [ deck[0], deck[2]]
 console.log("Welcome to Blackjack!")
 
 console.log("You are dealt: ")
