957601db30586271a2ffbea8a155fdcdbd2c4c1e
luucamay <lupemaydana@gmail.com>
2019-10-14 18:11:33 -0400

    Calculating the score

--- blackjack/lib/script.js
+++ blackjack/lib/script.js
@@ -65,10 +65,88 @@ function getCardString(card){
     return card.value + ' of ' + card.suit
 }
 
+function getCardNumericValue(card){
+    switch(card.value) {
+        case 'Ace':
+            return 1
+        case 'Two':
+            return 2
+        case 'Three':
+            return 3
+        case 'Four':
+            return 4
+        case 'Five':
+            return 5
+        case 'Six':
+            return 6
+        case 'Seven':
+            return 7
+        case 'Eight':
+            return 8
+        case 'Nine':
+            return 9
+        default:
+            return  10           
+    }
+}
+
+function getScore(cardArray){
+    let score = 0
+    let hasAce = false
+     for (let i = 0; i < cardArray.length; i++){
+         let card = cardArray[i]
+         score += getCardNumericValue(card)
+         if (card.value === 'Ace'){
+             hasAce = true
+         }
+     }
+     if(hasAce && score + 10 <= 21){
+         return score + 10
+     }
+    return score
+}
+
+function updateScores(){
+    dealerScore = getScore(dealerCards)
+    playerScore = getScore(playerCards)
+}
+
 function showStatus(){
-    if(!gameStarted)
+    if(!gameStarted){
         textArea.innerText = 'Welcome to Blackjack!'
-    return
+        return
+    }
+
+    let dealerCardString = ''
+    for (let i=0; i < dealerCards.length; i++){
+        dealerCardString += getCardString(dealerCards[i]) + '\n'   
+    }
+    let playerCardString = ''
+    for (let i = 0; i < playerCards.length; i++){
+        playerCardString += getCardString(playerCards[i]) + '\n'
+    }
+
+    updateScores()
+
+    textArea.innerText =
+        'Dealer has:\n' +
+        dealerCardString +
+        '(score: ' + dealerScore + ')\n\n' +
+
+        'Player has:\n' +
+        playerCardString +
+        '(score: ' + playerScore + ')\n\n'
+    
+        if(gameOver){
+            if(playerWon){
+                textArea.innerText += "YOU WIN!"
+            } else {
+                textArea.innerText += "DEALER WINS"
+            }
+            newGameButton.style.display = 'inline'
+            hitButton.style.display = 'none'
+            stayButton.style.display = 'none'
+        }
 }
 
 function getNextCard(){
