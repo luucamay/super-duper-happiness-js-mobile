f72f7d383d91ef068eacbc27568eccb6a32ff7e5
luucamay <lupemaydana@gmail.com>
2019-10-14 12:06:31 -0400

    Adding functions to blackjack

--- blackjack/lib/script.js
+++ blackjack/lib/script.js
@@ -2,18 +2,26 @@
 let suits = ["Hearts", "Clubs", "Diamonds", "Spiders"]
 let values = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two", "One"]
 
-let deck = []
-
-for(let i = 0; i < suits.length; i++){
-    for(let j = 0; j < values.length; j++){
-        deck.push(values[j] + " of " + suits[i])
+function createDeck() {
+    let deck = []
+    for(let i = 0; i < suits.length; i++){
+        for(let j = 0; j < values.length; j++){
+            deck.push(values[j] + " of " + suits[i])
+        }
     }
+    return deck    
 }
+
+function getNextCard(){
+    return deck.shift()
+}
+
+let deck = createDeck()
 
 for (let i = 0; i < deck.length; i++){
     console.log(deck[i])
 }
-let playerCards = [ deck[0], deck[2]]
+let playerCards = [ getNextCard(), getNextCard()]
 console.log("Welcome to Blackjack!")
 
 console.log("You are dealt: ")
